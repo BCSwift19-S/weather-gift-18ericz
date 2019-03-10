09c3ab7e2d36ff75626c94cb77c234ba95a911cb
马蕾 <18ericz@18ericzdembp.bc.edu>
2019-03-09 22:08:06 -0500

    Version 7.2

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by 18ericz on 3/9/19.
+//  Copyright © 2019 18ericz. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ WeatherGift/PageVC.swift
@@ -0,0 +1,21 @@
+//
+//  PageVC.swift
+//  WeatherGift
+//
+//  Created by 18ericz on 3/9/19.
+//  Copyright © 2019 18ericz. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
