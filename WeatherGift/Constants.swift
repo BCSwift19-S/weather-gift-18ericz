516aa4882e801bbab8940d3ebbcd99e20d18d230
马蕾 <18ericz@18ericzdembp.bc.edu>
2019-03-18 13:31:57 -0400

    Version 7.15

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by 18ericz on 3/18/19.
+//  Copyright © 2019 18ericz. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "b3036c55c2311f4558a61a7dab8ec60b/"
+
